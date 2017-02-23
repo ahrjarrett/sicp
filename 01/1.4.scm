ba33158667c07a392f606a2b47aa548375a4c22e
Andrew Jarrett <ahrjarrett@gmail.com>
2017-02-23 00:46:27 -0700

    refactored sqrt 01/1.4

--- 01/1.4.scm
+++ 01/1.4.scm
@@ -1,14 +1,29 @@
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    ; take the avg of guess and x divided by guess
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
       guess
-      (sqrt-iter (improve guess x)
-                x)))
+      (sqrt-iter (improve guess))))
+  (sqrt-iter 1))
 
-(define (improve guess x)
-  (avg guess (/ x guess)))
+(define (average num1 num2)
+  (/ (+ num1 num2) 2))
 
-(define (good-enough? guess x)
-  ()
+(display (sqrt 9))
 
-)
-
+;; Old attempt assigned variables to the global environment
+;(define (sqrt-iter guess x)
+;  (if (good-enough? guess x)
+;      guess
+;      (sqrt-iter (improve guess x)
+;                x)))
+;
+;(define (improve guess x)
+;  (avg guess (/ x guess)))
+;
+;(define (good-enough? guess x)
+;  ())
