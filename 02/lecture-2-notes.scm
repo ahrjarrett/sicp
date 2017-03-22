962589d7ad17a9d87829d759898f61c0196c1971
Andrew Jarrett <ahrjarrett@gmail.com>
2017-03-21 19:19:28 -0600

    3/21/17 finished 1st half of lecture 2a

--- 02/lecture-2-notes.scm
+++ 02/lecture-2-notes.scm
@@ -1,4 +1,5 @@
 ; TODO: Need to define or import square
+; BOOKMARK: 03/21/17 stopped at 22:30, halfway through lecture
 
 (define (sum-int a b)
   (if (> a b)
@@ -12,5 +13,82 @@
       (+ (square a)
          (sum-sq (1+ a) b))))
 
-(sum-int 2 5) ; 14
+(define (square x) x * x)
+
+;(sum-int 2 5) ; 14
 (sum-sq 5 8) ; 176
+
+; NOTES:
+; Anytime we have almost identical things like sum-int and sum-sq,
+; (or Leibnitz’ formula for computing PI over a)
+; we're going to have to come up with some sort of ABSTRACTION
+; to cover them.
+
+; usually you learn a number of idioms when learning a computer
+; language, common patterns of usage (gives the example of FORTRAN
+; problem of finding largest number in a set)
+
+; What we're doing instead is giving a name to that type of knowledge
+; The pattern is:
+
+(define (<name> a b)
+  (if (> a b)
+      0
+      (+ (<term> a)
+         (<name> (<next> a) b))))
+
+; Numbers are just one kind of data.
+; Procedures are simply a name we’ve given to a data
+; that behaves a particular way
+
+; So as an abstraction:
+
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term
+              (next a)
+              next ; kinda confused about next here (see 12:00 - 14:00 min)
+              b))))
+
+; weird how we define it inside of sum-int, IMO
+; why don't we put identity a inside parens?
+; and how does this do the same thing as sum-int?
+
+(define (sum-int-2 a b)
+  (define (identity a) a)
+  (sum identity a 1+ b))
+
+; Ohh now I get it. We defined sum, so sum-int-2
+; is simply figuring out which args to pass to the
+; SUM function (the trickiest part seems to be
+; figuring out what the term is; in the case of sum-int-2,
+; the term is the IDENTITY function
+
+(define (sum-sq-2 a b)
+  (sum square a 1+ b))
+
+
+; (LAMBDA) : first example of an anonymous function!
+(define (pi-sum a b)
+  (sum (LAMBDA (i) (/ 1 (* i (+ i 2))))
+       a
+       (LAMBDA (i) (+ i 4))
+       b))
+
+
+; here’s an iterative interpretation of SUM
+; sometimes might be better (but seems to fly
+; in the face of the whole LISP philosophy
+(define (sum-iter term a next)
+  (define (iter j ans)
+    (if (> j b)
+        ans
+        (iter (next j)
+              (+ (term j) ans))))
+  (iter a ))
+
+
+
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Problem Sets: SICP
 
-These are the solutions for the problem sets in “Structure and Interpretation of Computer Programs”.
+These are notes and solutions to the problemsets from [“Structure and Interpretation of Computer Programs”](https://mitpress.mit.edu/sicp/full-text/book/book.html).
 
-TODO:
+## TODO:
 -~~Run files from emacs, edit them in Vim?~~
 -Run files from REPL for now with `(load "filename.scm")`
+
+### TOC:
+1. Solutions to Chapter 1 Problemsets (1-10)
+2. Notes on Lectures 2a and 2b
+3. Solutions to Chapter 3 Problemsets
